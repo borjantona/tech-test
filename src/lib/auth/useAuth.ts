dc2118ce609564f21ad53ef8b064c7980841a0c7
Borjuto <borja.antona@gmail.com>
2024-12-30 11:12:12 +0100

    - Added calls to api login

--- src/lib/auth/useAuth.ts
+++ src/lib/auth/useAuth.ts
@@ -1,4 +1,5 @@
 import { Auth } from './types'
+import { useApiFetcher } from "../api";
 
 /**
  * Returns the current auth state. See {@link Auth} for more information on
@@ -7,12 +8,29 @@ import { Auth } from './types'
  * @throws {TypeError} if called from a component not descendant of AuthProvider
  */
 function useAuth(): Auth {
+   const fetcher = useApiFetcher();
+
   return {
     tokens: null,
     currentUser: null,
-    login(credentials) {
-      const { email, password } = credentials
-      return Promise.reject(new Error('Not yet implemented'))
+    async login(credentials) {
+      const { email, password } = credentials;
+	  const res = await fetcher("POST /v3/auth/login", {
+        data: { email, password },
+      });
+      if (!res.ok) {
+        throw new Error(res.data.message);
+      }
+      if (
+        typeof res.data.accessToken === "string" &&
+        typeof res.data.accessTokenExpiresAt === "string" &&
+        typeof res.data.refreshToken === "string" &&
+        typeof res.data.refreshTokenExpiresAt === "string"
+      ) {
+		return Promise.resolve();
+      } else {
+        throw new Error("Not matching data from the API.");
+      }
     },
     logout() {
       return Promise.reject(new Error('Not yet implemented'))
